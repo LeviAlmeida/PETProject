14c512cf5c7b351c811126881726319895a9c68e
Marcos Vinicius Sombra <markos.vs@live.com>
2018-10-04 09:58:06 -0300

    Create traitor2.py

---
+++ traitor2.py
@@ -0,0 +1,10 @@
+print('denis é traidor')
+print('denis tem que se fuder e acabou')
+print('uau')
+print('lala')
+print('lala')
+for i in range(10):
+    print(i)
+
+for i in range(10):
+    print(i)
